cea78bc08e1a236be52c3b3d9df2f6547d4f841b
alexandre <alexandre020602@gmail.com>
2026-02-16 17:51:21 -0300

    config biome

--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,37 @@
-import fastify from "fastify";
 import { fastifyCors } from '@fastify/cors'
-import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTransform } from "fastify-type-provider-zod"
-import { fastifySwagger } from "@fastify/swagger"
-import { fastifySwaggerUi } from "@fastify/swagger-ui"
-import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
-import { env } from "./env";
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { subscribeToEventRoute } from './routes/subscribe-to-event-route'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, { origin: true })
 
-app.setValidatorCompiler(validatorCompiler);
-app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
 
 app.register(fastifySwagger, {
-    openapi: {
-        info: {
-            title: "Fastify API",
-            description: "API documentation",
-            version: "0.1.0"
-        }
+  openapi: {
+    info: {
+      title: 'Fastify API',
+      description: 'API documentation',
+      version: '0.1.0',
     },
-    transform: jsonSchemaTransform
+  },
+  transform: jsonSchemaTransform,
 })
 
-app.register(fastifySwaggerUi, { routePrefix: "/docs" })
+app.register(fastifySwaggerUi, { routePrefix: '/docs' })
 app.register(subscribeToEventRoute)
 
-
-
 app.listen({ port: env.PORT }).then(() => {
-    console.log("HTTP server running")
+  console.log('HTTP server running')
 })
