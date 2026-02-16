fb9dde70934b22b4dd5166df5d668e31c4335ee6
alexandre <alexandre020602@gmail.com>
2026-02-16 17:18:35 -0300

    envSchema with zod + add in package.json envfile

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { validatorCompiler, serializerCompiler, ZodTypeProvider, jsonSchemaTrans
 import { fastifySwagger } from "@fastify/swagger"
 import { fastifySwaggerUi } from "@fastify/swagger-ui"
 import { subscribeToEventRoute } from "./routes/subscribe-to-event-route";
+import { env } from "./env";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -28,6 +29,6 @@ app.register(subscribeToEventRoute)
 
 
 
-app.listen({ port: 3333 }).then(() => {
+app.listen({ port: env.PORT }).then(() => {
     console.log("HTTP server running")
 })
