b9402f409e7b473b8da7108b6feed59f45b854c2
alexandre <alexandre020602@gmail.com>
2026-02-18 16:49:48 -0300

    validation

--- src/functions/subscribe-to-event.ts
+++ src/functions/subscribe-to-event.ts
@@ -1,12 +1,23 @@
+import { eq } from "drizzle-orm";
 import { db } from "../drizzle/client";
-import {subscriptions} from "../drizzle/schema/subscriptions"
+import { subscriptions } from "../drizzle/schema/subscriptions"
 
 interface SubscriptionToEventParams {
     name: string;
     email: string;
 }
 
-export async function subscribeToEvent({name, email}: SubscriptionToEventParams) {
+export async function subscribeToEvent({ name, email }: SubscriptionToEventParams) {
+
+    const subscribers = await db.select()
+        .from(subscriptions)
+        .where(eq(subscriptions.email, email))
+
+    if (subscribers.length > 0) {
+        return {
+            subscriberId: subscribers[0].id
+        }
+    }
 
     const result = await db.insert(subscriptions).values({
         name,
