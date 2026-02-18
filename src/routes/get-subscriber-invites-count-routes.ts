53eb49216b39aaa814b5ddd067089dd6d023c703
alexandre <alexandre020602@gmail.com>
2026-02-18 19:42:43 -0300

    route invitesCounter

---
+++ src/routes/get-subscriber-invites-count-routes.ts
@@ -0,0 +1,32 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import { getSubscriberInviteClick } from '../functions/get-subscriber-invite-click'
+import { z } from 'zod'
+import { getSubscriberInviteCount } from '../functions/get-subscriber-invites-count'
+
+
+export const getSubscriberInviteCountRoute: FastifyPluginAsyncZod = async app => {
+  app.get(
+    '/subscribers/:subscriberId/ranking/count',
+    {
+      schema: {
+        summary: 'Get subscriber invites count',
+        tags: ['referral'],
+        params: z.object({
+            subscriberId: z.string(),
+        }),
+        response: {
+            200: z.object({
+                count: z.number(),
+            }),
+          },
+        },
+      },
+
+    async (request) => {
+
+        const { subscriberId } = request.params
+
+        return await getSubscriberInviteCount({ subscriberId})
+    },
+  )
+}
